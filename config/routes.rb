b33f6d907b57ae3f85a73b130d506c6a20d0ff90
hironori_megumi <akamarl.1127@gmail.com>
2017-06-08 11:35:22 +0900

    おうむ返し

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/line_bots/callback' => 'line_bots#callback'
 end
